402960631ee946ab5b2540db3e35caf25ba113d1
papa31 <visidoor@yandex.ru>
2020-06-05 23:14:17 +0500

    Change index.js again

--- my-datocms-project/pages/index.js
+++ my-datocms-project/pages/index.js
@@ -1,71 +1,18 @@
 import { request } from '../lib/datocms'
-import { Image, renderMetaTags } from 'react-datocms'
-import Head from 'next/head'
-
-const HOMEPAGE_QUERY = `
-query HomePage($limit: IntType) {
-  site: _site {
-    favicon: faviconMetaTags {
-      attributes
-      content
-      tag
-    }
-  }
-  blog {
-    seo: _seoMetaTags {
-      attributes
-      content
-      tag
-    }
-  }
-  allPosts(first: $limit) {
-    id
+const HOMEPAGE_QUERY = `query HomePage($limit: IntType) {
+  allBlogPosts(first: $limit) {
     title
-    excerpt
-    date
-    author {
-      name
-    }
-    coverImage {
-      responsiveImage(imgixParams: { fit: crop, w: 300, h: 300, auto: format }) {
-        srcSet
-        webpSrcSet
-        sizes
-        src
-        width
-        height
-        aspectRatio
-        alt
-        title
-        base64
-      }
-    }
   }
 }`
-
 export async function getStaticProps() {
   const data = await request({
     query: HOMEPAGE_QUERY,
     variables: { limit: 10 },
   })
-
   return {
-    props: {
-      data,
-    },
+    props: { data },
   }
 }
-
 export default function Home({ data }) {
-  return (
-    <div>
-      <Head>{renderMetaTags(data.blog.seo.concat(data.site.favicon))}</Head>
-      {data.allPosts.map((blogPost) => (
-        <article key={blogPost.id}>
-          <Image data={blogPost.coverImage.responsiveImage} />
-          <h6>{blogPost.title}</h6>
-        </article>
-      ))}
-    </div>
-  )
+  return <div>{JSON.stringify(data, null, 2)}</div>
 }
